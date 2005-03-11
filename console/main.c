9884214a148974f6c0394c5299304f1ab9cfd965
dmouldin <dmouldin>
2005-03-11 23:32:52 +0000

    Adjusted the size of the malloc'd block.

--- console/main.c
+++ console/main.c
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  $Id: main.c,v 1.1 2005/03/11 22:56:50 dmouldin Exp $
+//  $Id: main.c,v 1.2 2005/03/11 23:32:52 dmouldin Exp $
 //
 //  A simple C example program for testing the Visual Leak Detector.
 //
@@ -11,7 +11,7 @@
 
 int main (int argc, char *argv [])
 {
-    char *s = (char*)malloc(60);
+    char *s = (char*)malloc(14);
 
     strncpy(s, "Hello World!\n", 14);
     printf(s);
