c25df0d53643faa2c8586db98333f1d033e57c78
db <db>
2005-03-31 02:09:19 +0000

    Made some cosmetic changes to comments.

--- vld.h
+++ vld.h
@@ -1,20 +1,20 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  $Id: vld.h,v 1.2 2005/03/29 14:18:10 db Exp $
+//  $Id: vld.h,v 1.3 2005/03/31 02:09:19 db Exp $
 //
-//  Visual Leak Detector Header (Version 0.9d)
+//  Visual Leak Detector (Version 0.9d)
 //  Copyright (c) 2005 Dan Moulding
 //
 //  This program is free software; you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser Public License as published by
+//  it under the terms of the GNU Lesser General Public License as published by
 //  the Free Software Foundation; either version 2.1 of the License, or
 //  (at your option) any later version.
 //
 //  This program is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser Public License for more details.
+//  GNU Lesser General Public License for more details.
 //
-//  You should have received a copy of the GNU Lesser Public License
+//  You should have received a copy of the GNU Lesser General Public License
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
