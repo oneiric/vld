377d0e02c0fd92325ee63ace319b7c83d11f2479
db <db>
2005-04-04 13:08:06 +0000

    Simplified configuration interface by using global variables defined in vld.h instead of using class members and friend functions.

--- vld.h
+++ vld.h
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  $Id: vld.h,v 1.3 2005/03/31 02:09:19 db Exp $
+//  $Id: vld.h,v 1.4 2005/04/04 13:08:06 db Exp $
 //
 //  Visual Leak Detector (Version 0.9d)
 //  Copyright (c) 2005 Dan Moulding
@@ -18,7 +18,7 @@
 //  along with this program; if not, write to the Free Software
 //  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 //
-//  See COPYING.txt for the full terms of the GNU Lesser Public License.
+//  See COPYING.txt for the full terms of the GNU Lesser General Public License.
 //
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,8 +26,6 @@
 #define _VLD_H_
 
 #ifdef _DEBUG
-
-#include <cassert>
 
 // Link with the appropriate Visual Leak Detector library. One of: multithreaded
 // DLL, multithreaded static, or single threaded. All three link with debug
@@ -47,97 +45,42 @@
 // though nobody directly references it outside of the library.
 #pragma comment(linker, "/include:?visualleakdetector@@3VVisualLeakDetector@@A")
 
-// Friend functions of the VisualLeakDetector class. These functions provide an
-// interface with the Visual Leak Detector library for configuring options.
-#ifdef __cplusplus
-extern "C"
-{
-#endif // __cplusplus
-void VLDSetMaxDataDump (unsigned long bytes);
-void VLDSetMaxTraceFrames (unsigned long frames);
-void VLDShowUselessFrames (unsigned int show);
-#ifdef __cplusplus
-}
-#endif // __cplusplus
-
-#ifdef __cplusplus
-
 ////////////////////////////////////////////////////////////////////////////////
 //
-//  The VisualLeakDetectorConfigurator Class
+// Configuration Options
 //
-//    This class exists solely to configure the Visual Leak Detector at runtime.
-//    A single global instance of this class is constructed. The constructor
-//    configures the Visual Leak Detector library according to the preprocessor
-//    macro configuration options that have been defined (if any).
-//
-class VisualLeakDetectorConfigurator {
-public:
-    VisualLeakDetectorConfigurator () {
-        static bool already_instantiated = false;
 
-        // Disallow more than one instantiation of the configurator.
-        // If this asserting, then you've included vld.h more than once.
-        // Include it in only ONE source file.
-        assert(!already_instantiated);
-        already_instantiated = true;
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
 
-        // If any of the preprocessor macros are defined, set the configurator
-        // up to configure those options at runtime. This allows the library
-        // (which is already compiled) to be configured using the convenience
-        // of the preprocessor macros, without having to modify any of the
-        // sources being debugged (aside from #including this header).
+// If VLD_MAX_DATA_DUMP is defined, then the amount of data shown in user-data
+// memory dumps will be limited to the specified number of bytes.
 #ifdef VLD_MAX_DATA_DUMP
-        VLDSetMaxDataDump(VLD_MAX_DATA_DUMP);
+unsigned long _VLD_maxdatadump = VLD_MAX_DATA_DUMP;
+#else
+unsigned long _VLD_maxdatadump = 0xffffffff;
 #endif // VLD_MAX_DATA_DUMP
 
+// If VLD_MAX_TRACE_FRAMES is defined, then the number of frames traced for each
+// allocated memory block when walking the stack will be limited to the
+// specified number of frames.
 #ifdef VLD_MAX_TRACE_FRAMES
-        VLDSetMaxTraceFrames(VLD_MAX_TRACE_FRAMES);
+unsigned long _VLD_maxtraceframes = VLD_MAX_TRACE_FRAMES;
+#else
+unsigned long _VLD_maxtraceframes = 0xffffffff;
 #endif // VLD_MAX_TRACE_FRAMES
 
+// If VLD_SHOW_USELESS_FRAMES is defined, then all frames traced will be
+// displayed, even frames internal to the heap and Visual Leak Detector.
 #ifdef VLD_SHOW_USELESS_FRAMES
-        VLDShowUselessFrames(1);
+unsigned char _VLD_showuselessframes = 0x1;
+#else 
+unsigned char _VLD_showuselessframes = 0x0;
 #endif // VLD_SHOW_USELESS_FRAMES
-    }
-};
-static VisualLeakDetectorConfigurator visualleakdetectorconfigurator;
 
-#else // __cplusplus
-
-// VLDConfigure - Configures the Visual Leak Detector library according to
-//   options specified via preprocessor macros.
-//
-//  Note: C programs will need to call this function if they want to override
-//    the default configuration via the preprocessor macros. For C++ programs
-//    calling this function is not necessary (the configurator object does it
-//    automatically).
-//
-//  Return Value:
-//
-//    None.
-//
-void VLDConfigure () {
-    static unsigned int already_configured = 0;
-
-    // Disallow more than one configuration.
-    // If this asserting, then you've called VLDConfigure() more than once.
-    // Call it only ONCE from ONE source file.
-    assert(!already_configured);
-    already_configured = 1;
-
-#ifdef VLD_MAX_DATA_DUMP
-    VLDSetMaxDataDump(VLD_MAX_DATA_DUMP);
-#endif // VLD_MAX_DATA_DUMP
-
-#ifdef VLD_MAX_TRACE_FRAMES
-    VLDSetMaxTraceFrames(VLD_MAX_TRACE_FRAMES);
-#endif // VLD_MAX_TRACE_FRAMES
-
-#ifdef VLD_SHOW_USELESS_FRAMES
-    VLDShowUselessFrames(1);
-#endif // VLD_SHOW_USELESS_FRAMES
+#ifdef __cplusplus
 }
-
 #endif // __cplusplus
 
 #endif // _DEBUG
