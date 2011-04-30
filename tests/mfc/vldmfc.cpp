5d825fd1d0b3c4257de3878601e20815e682e807
Chris Johnson <christopher3d@hotmail.com>
2011-04-30 11:14:03 +0000

    A little clean up of the unit tests. Added the already existing vldmfc application to the test folder in visual studio. Ran all the tests before submitting. testsuite.exe is still broken, and still throwing an assert during execution.
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/vld/vld/trunk;C66359

--- tests/mfc/vldmfc.cpp
+++ tests/mfc/vldmfc.cpp
@@ -48,7 +48,7 @@ BOOL CMFCExampleApp::InitInstance()
 
     CMFCExampleDlg dlg;
     m_pMainWnd = &dlg;
-    int nResponse = dlg.DoModal();
+    INT_PTR nResponse = dlg.DoModal();
     if (nResponse == IDOK)
     {
         // TODO: Place code here to handle when the dialog is
