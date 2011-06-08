79b0d9dd37b03a7c94d8dbababd102f96cb0bfe9
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2011-06-08 12:17:45 +0000

    Use WideCharToMultiByte fixed
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/vld/vld/trunk;C67637

--- vld.cpp
+++ vld.cpp
@@ -2008,15 +2008,15 @@ BOOL VisualLeakDetector::addloadedmodule (PCWSTR modulepath, DWORD64 modulebase,
         vld.m_vldbase = (HMODULE)modulebase;
     }
     else {
-        // Convert the module path to ASCII.
-        length = wcslen(modulename) + 1;
-        LPSTR modulenamea = new CHAR [length];
+		// Convert the module path to ASCII.
+		length = ::WideCharToMultiByte(CP_ACP, 0, modulename, -1, 0, 0, 0, 0);
+		LPSTR modulenamea = new CHAR [length];
 
         // wcstombs_s requires locale to be already set up correctly, but it might not be correct on vld init step. So use WideCharToMultiByte instead
         CHAR defaultChar     = '?';
         BOOL defaultCharUsed = FALSE;
 
-        int count = ::WideCharToMultiByte(CP_ACP, 0/*flags*/, modulename, (int)length, modulenamea, (int)length, &defaultChar, &defaultCharUsed);
+        int count = ::WideCharToMultiByte(CP_ACP, 0/*flags*/, modulename, (int)-1, modulenamea, (int)length, &defaultChar, &defaultCharUsed);
         assert(count != 0);
         if ( defaultCharUsed )
         {
