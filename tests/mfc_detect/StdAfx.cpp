a1701172f0142571440e76514ed85dc32aa04be9
Arkadiy Shapkin <arkadiy_s@inbox.ru>
2011-06-18 16:26:40 +0000

    Removed VS2008 projects
    git-tfs-id: [https://tfs.codeplex.com/tfs/TFS07]$/vld/vld/trunk;C67951

--- tests/mfc_detect/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-//  source file that includes just the standard includes
-//      vldmfc.pch will be the pre-compiled header
-//      stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
