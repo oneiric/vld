8d636a4ef5dae48f2660fbe8f9a8a1a74859ec39
Arkadiy Shapkin <ashapkin@artec-group.com>
2013-04-29 03:45:23 +0400

    Version changed to 2.3

--- setup/version.h
+++ setup/version.h
@@ -1,9 +1,9 @@
 
-#define VLDVERSION          L"2.2.3"
-#define VERSION_NUMBER		2,2,3,0
-#define VERSION_STRING		"2.2.3.0"
+#define VLDVERSION          L"2.3"
+#define VERSION_NUMBER		2,3,0,0
+#define VERSION_STRING		"2.3.0.0"
 #define VERSION_COPYRIGHT	"Copyright (C) 2005-2013"
 
 #ifndef __FILE__
-!define VLD_VERSION "2.2.3"	// NSIS Script
+!define VLD_VERSION "2.3"	// NSIS Script
 #endif
