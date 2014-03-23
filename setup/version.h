1f6333cfe220208fe5fa91157a736c92e25fdf42
Arkady Shapkin <arkady.shapkin@gmail.com>
2014-03-24 00:38:58 +0400

    Change log updated

--- setup/version.h
+++ setup/version.h
@@ -1,9 +1,9 @@
 
-#define VLDVERSION          L"2.4"
+#define VLDVERSION          L"2.4RC"
 #define VERSION_NUMBER		2,4,0,0
 #define VERSION_STRING		"2.4.0.0"
 #define VERSION_COPYRIGHT	"Copyright (C) 2005-2014"
 
 #ifndef __FILE__
-!define VLD_VERSION "2.4"	// NSIS Script
+!define VLD_VERSION "2.4RC"	// NSIS Script
 #endif
--- setup/vld-setup.iss
+++ setup/vld-setup.iss
@@ -2,7 +2,7 @@
 ; SEE THE DOCUMENTATION FOR DETAILS ON CREATING INNO SETUP SCRIPT FILES!
 
 #define MyAppName "Visual Leak Detector"
-#define MyAppVersion "2.4"
+#define MyAppVersion "2.4rc"
 #define MyAppPublisher "VLD Team"
 #define MyAppURL "http://vld.codeplex.com/"
 #define MyAppRegKey "Software\Visual Leak Detector"
