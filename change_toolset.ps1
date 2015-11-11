d1c9f273c5b0d67b5ba905fa19fc6b1dab8d6159
Arkady Shapkin <arkady.shapkin@gmail.com>
2015-11-11 23:26:22 +0300

    Added force parameter to change_toolset.ps1

--- change_toolset.ps1
+++ change_toolset.ps1
@@ -1,11 +1,12 @@
 Param(
     [Parameter(Mandatory=$True,Position=1)]
-    [string]$toolset
+    [string]$toolset,
+    [switch]$force
 )
 
 $files = Get-ChildItem -path . -filter *.vcxproj -recurse
 foreach ($file in $files) {
-    if (($file.basename -eq "vld") -or ($file.basename -eq "format"))
+    if (!$force.IsPresent -and (($file.basename -eq "vld") -or ($file.basename -eq "format")))
     {
         "Skip $($file.name)"
         continue
@@ -22,5 +23,23 @@ foreach ($file in $files) {
     else
     {
         "Toolset not set for $($file.name)"
+    }
+}
+
+$commonProps = ".\src\tests\Common.props"
+if ($force.IsPresent -and (Test-Path $commonProps))
+{
+    $content = gc $commonProps
+    $regex = [regex]"<VldToolset>(v[\d\w_]+)<\/VldToolset>"
+    $oldtoolset = $regex.matches($content)
+    if ($oldtoolset.groups.count -ge 1)
+    {
+        "  Common.props ($($oldtoolset.groups[1].value) - $toolset)"
+        $content = $content -replace "<VldToolset>(v[\d\w_]+)<\/VldToolset>","<VldToolset>$toolset</VldToolset>"
+        $content | sc $commonProps
+    }
+    else
+    {
+        "Toolset not set for $commonProps"
     }
 }
